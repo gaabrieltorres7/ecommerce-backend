9a1e7140a8bbf2571d5e294eaa1f38926cb05fb5
Gabriel Torres <gaabrieltt7@gmail.com>
2025-06-14 17:10:37 -0300

    chore: change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,6 +35,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
     },
   },
 );
